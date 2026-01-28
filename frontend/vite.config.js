bd28575011fdfc3a7dd1b45079bbfd3ffc269dc9
eros-codes <saghafiarvin@gmail.com>
2026-01-28 10:51:08 +0330

    update build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './frontend',
+  root: '.',
   plugins: [react()],
   build: {
     outDir: '../dist',
