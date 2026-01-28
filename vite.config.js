aef775ca20258f4d9516372f7eca2ce90550113d
eros-codes <saghafiarvin@gmail.com>
2026-01-28 13:39:58 +0330

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: './frontend',
+  base: '/todo/',
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: '../docs',
     emptyOutDir: true
   },
   server: {
